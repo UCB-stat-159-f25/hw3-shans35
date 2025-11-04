55f952f7f740e70bc5cc064e926c609cd33af894
shans35 <sophie.hanson35@gmail.com>
2025-11-04 18:25:31 +0000

    add test whiten

---
+++ ligotools/tests/test_utils.py
@@ -0,0 +1,13 @@
+from ligotools.utils import whiten, write_wavfile, reqshift, make_psd_window
+import numpy as np
+
+def test_whiten():
+    dt=np.float64(0.000244140625)
+    strain=np.sin(2*np.pi*50*np.arange(0, 1, dt)) # make a test sine wave
+    interp_psd=lambda f: np.ones_like(f) # takes f and returns 1 for every frequency
+
+    whiten_test = whiten(strain, interp_psd, dt)
+
+    assert(len(whiten_test) == len(strain) # assert the output is the same length as the input
+    assert np.all(np.isfinite(white_ht)) # assert there are no NaN numbers
+
